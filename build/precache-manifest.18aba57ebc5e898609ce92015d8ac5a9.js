bf0cdbd4de1059d2ce00de4168ec0943320e0a9d
Jaqueline_Araujo <jaquelineluiza4444@gmail.com>
2020-10-16 23:34:50 -0300

    Inicializando com Heroku

---
+++ build/precache-manifest.18aba57ebc5e898609ce92015d8ac5a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b7647bf9887ff6f0e5a30f4815ce4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3f4f5d06eb9aa9aa25e",
+    "url": "/static/css/main.0ae9a398.chunk.css"
+  },
+  {
+    "revision": "0b39ced750a11347cbfb",
+    "url": "/static/js/2.0ae3dde6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0ae3dde6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f4f5d06eb9aa9aa25e",
+    "url": "/static/js/main.d206b146.chunk.js"
+  },
+  {
+    "revision": "21a683f7f40fceed12f4",
+    "url": "/static/js/runtime-main.b6dad71e.js"
+  }
+]);
